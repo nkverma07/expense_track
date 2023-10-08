20ab480f5a7ca070dedf41953a7e5b658f5eb4f6
Narender Kumar <narenderkumar.hut@gmail.com>
2023-10-08 21:46:35 +0530

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:expense_track/expenses.dart';
+import 'package:expense_track/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,4 @@
-import 'package:expense_track/expenses_list.dart';
+import 'package:expense_track/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_track/models/expense.dart';
 import 'package:flutter/material.dart';
 
