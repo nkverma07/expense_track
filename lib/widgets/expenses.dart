316a15aa29137d4e3e4e4bd64199dd54de061188
Narender Kumar <narenderkumar.hut@gmail.com>
2023-10-09 23:03:01 +0530

    day3 completed

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_track/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_track/models/expense.dart';
+import 'package:expense_track/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -37,10 +38,23 @@ class _ExpensesState extends State<Expenses> {
       category: Category.travel,
     )
   ];
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Expanses App"),
+        actions: [
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add)),
+        ],
+      ),
       body: Column(
         children: [
           const Text("Chart"),
