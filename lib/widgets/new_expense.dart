801240c3580a2a0b32d5f5420856632cae2ddb92
Narender Kumar <narenderkumar.hut@gmail.com>
2023-10-12 17:45:02 +0530

    day 6 completed

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:expense_track/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() => _NewExpenseState();
@@ -49,7 +51,13 @@ class _NewExpenseState extends State<NewExpense> {
                       child: const Text("Okey"))
                 ],
               ));
+      return;
     }
+    widget.onAddExpense(Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        dateTime: _selectedDate!,
+        category: _selectedCategory));
   }
 
   @override
